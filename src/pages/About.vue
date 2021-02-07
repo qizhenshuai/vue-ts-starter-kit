adb547125e1b6a6196f26a613fc3436b1662ef75
qizhenshuai <qizhenshuai@126.com>
2021-02-07 15:22:53 +0800

    test: vuex

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <van-button type="primary" @click="handleUpdateVersion">关于版本</van-button>
   </div>
 </template>
+
+<script lang="ts">
+import { useStore } from 'vuex'
+export default {
+  name: 'about',
+  setup() {
+    const store = useStore()
+    const handleUpdateVersion = () => {
+      store.dispatch('app/UPDATE_VERSION_SYNC', { version: '2.x' })
+    }
+    return {
+      handleUpdateVersion
+    }
+  }
+}
+</script>
